b99fea17efd6bb3af42074af0110814b39675cf7
NetanPa <netanelpashur@gmail.com>
2026-06-16 21:32:46 +0300

    started working on the command file (I need an explnation please)

---
+++ Command.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "arp_utils.h"
+#include "utils.h"
+
+extern bool verbose;
+
+class Command {
+	public:
+		// Constructor
+		Command(bool isFirstRun,);
+		void removeHost(std::string hostName);
+		void changeHostName(std::string currName, std::string newName);
+	protected:
+		void updateHosts();
+	private:
+		bool isFirstRun; //what is this for?
+		std::vector<host_info> chosenHosts; 
+};
