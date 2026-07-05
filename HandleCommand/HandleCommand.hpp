4db796238b432ee7d9763262c98cae76fe77d7cd
RoeeWork <roeework2804@gmail.com>
2026-07-05 15:50:19 +0300

    added HandleCommand

---
+++ HandleCommand/HandleCommand.hpp
@@ -0,0 +1,36 @@
+#include "../utils.h"
+
+/* HandleCommand: includes endpoints for each command in `rsc`.
+ *				  for example: removeHost is the endpoint
+ *				  for `$ rsc --remove`.
+ *				  
+ *				  WARNING: this class is only ment to be used
+ *				  		   in the main function. Do not use this
+ *				  		   classes methods inside any other class
+ *				  		   other than main.
+ */
+
+class HandleCommand {
+	public:
+		// Prompts user with the hosts that are known on
+		// the LAN, and adds the chosen ones into the DB.
+		static void addMultipleHosts();
+
+		// adds a single host using an IPv4 address.
+		static void addByIPv4(std::string ipv4);
+
+		// adds a single host using an MAC address.
+		static void addByMAC(std::string mac);
+
+		// removes a single host using it's name.
+		static void removeHost(std::string hostName);
+
+		// chages a host name from oldName into newName.
+		static void changeHostName(std::string oldName, std::string newName);
+
+		// displays saved hosts with an updated `status` field.
+		static void printHosts();
+
+		// resets rsc into default settings.
+		static void factoryReset();
+};
