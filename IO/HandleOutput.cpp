e6f9cce8303dbe16f393b7443e440ecf377871a9
RoeeWork <roeework2804@gmail.com>
2026-07-13 18:18:26 +0300

    added printNumberedTableHeader() and printNumberedTableHostRow().

--- IO/HandleOutput.cpp
+++ IO/HandleOutput.cpp
@@ -30,33 +30,64 @@ DESCRIPTION:
 
 // prints the host vector `hosts` in a numbered list.
 // only prints IPv4 and MAC addresses.
-void HandleOutput::printHostVectorNumberedList(std::vector<Host> hosts) {
-int i = 1;
-std::cout << "\n";
-std::cout << BOLD 
-		  << std::left
-		  << std::setw(4) << "#"
-		  << std::setw(15) << "IPV4"
-		  << std::setw(20) << "MAC"
-		  << RESET
-		  << '\n';
+void HandleOutput::printHostVectorNumberedTable(std::vector<Host> hosts) {
+	int hostIndex = 1;
+	// TODO: change these to a struct called TableContext? could
+	// 		 be useful for printing other tables... and will make
+	// 		 it easier to use the printTableHeader() and the
+	// 		 printTableHostColumn() methods.
+	int rowNumberColumnMargin = 4;
+	int IPv4ColumnMargin = 15;
+	int MACColumnMargin = 20;
 
-for (const auto &h : hosts) {
-	std::cout << std::left
-			  << std::setw(4) << i << RESET
-			  << GREEN
-			  << std::setw(15) << h.getIPv4()
-			  << std::setw(20) << h.getMAC()<< RESET
-			  << '\n';
-	i++;
+	printNumberedTableHeader(rowNumberColumnMargin, IPv4ColumnMargin, MACColumnMargin);
+	for (auto &h : hosts) {
+		printNumberedTableHostRow(hostIndex, h, rowNumberColumnMargin, IPv4ColumnMargin, MACColumnMargin);
+		hostIndex++;
+	}
 }
+
+// prints the numbered host table header.
+// TODO: seems unintuitive.
+void  HandleOutput::printNumberedTableHeader(int rowNumberColumnMargin, int IPv4ColumnMargin, int MACColumnMargin) {
+	//
+	// #   IPV4           MAC	<---------- prints this!
+	// 1   <ipv4_no1>	  <mac_no1>
+	// 2   <ipv4_no2>	  <mac_no2>
+	// .....
+	//
+	std::cout << "\n";
+	std::cout << BOLD 
+			  << std::left
+			  << std::setw(rowNumberColumnMargin) << "#"
+			  << std::setw(IPv4ColumnMargin) << "IPV4"
+			  << std::setw(MACColumnMargin) << "MAC"
+			  << RESET
+			  << '\n';
+}
+
+// prints a new row to the numbered table.
+// TODO: seems unintuitive.
+void HandleOutput::printNumberedTableHostRow(int rowNumber, Host& h, int  rowNumberColumnMargin, int IPv4ColumnMargin, int MACColumnMargin) {
+	//
+	// #   IPV4           MAC	
+	// 1   <ipv4_no1>	  <mac_no1> <-------- prints this!
+	// 2   <ipv4_no2>	  <mac_no2> <-------- prints this!
+	// .....
+	//
+	std::cout << std::left
+			  << std::setw(rowNumberColumnMargin) << rowNumber << RESET
+			  << GREEN     
+			  << std::setw(IPv4ColumnMargin) << h.getIPv4()
+			  << std::setw(MACColumnMargin) << h.getMAC()<< RESET
+			  << '\n';
 }
 
 std::vector<Host> HandleOutput::StartChooseHostsLoop(std::vector<Host> foundHosts) {
 	int hostNum;
 	std::vector<Host> chosenAddrs;
 	while (true) {
-		printHostVectorNumberedList(foundHosts);
+		printHostVectorNumberedTable(foundHosts);
 
 		std::cout << "[AddServers::Start()] choose host number (-1 if done): ";
 		std::string num;
--- IO/HandleOutput.hpp
+++ IO/HandleOutput.hpp
@@ -14,9 +14,17 @@ class HandleOutput {
 
 		static std::vector<Host> StartChooseHostsLoop(std::vector<Host> foundHosts);
 
-		// pretty prints hosts in chosenHosts as a list.
+		// pretty prints hosts in chosenHosts as a numbered table.
 		// only prints ipv4 and mac.
-		static void printHostVectorNumberedList(std::vector<Host> hosts); 
+		static void printHostVectorNumberedTable(std::vector<Host> hosts); 
+
+		// prints the numbered host table header.
+		// TODO: seems unintuitive.
+		static void printNumberedTableHeader(int  rowNumberColumnMargin, int IPv4ColumnMargin, int MACColumnMargin);
+
+		// prints a new row to the numbered table.
+		// TODO: seems unintuitive.
+		static void printNumberedTableHostRow(int rowNumber, Host& h, int  rowNumberColumnMargin, int IPv4ColumnMargin, int MACColumnMargin);
 
 		// prints msg if verbose is equal to true. 
 		static void verbosePrint(const std::string& msg, bool verbose);
